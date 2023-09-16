3fb37e0dd4b3db2497a2de2e9f81d04215a7276c
daiki_win <kubotadaiki0654@gmail.com>
2023-09-16 17:37:43 +0900

    feat: windowsで一通り動作するコードの完成

--- extension/background.js
+++ extension/background.js
@@ -3,71 +3,95 @@ async function t() {
   return (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0];
 }
 
+// 入力するkey情報を設定
+const allSelect = {
+  type: "keyDown",
+  windowsVirtualKeyCode: 65,
+  modifiers: 2,
+};
+const copy = {
+  type: "keyDown",
+  windowsVirtualKeyCode: 67,
+  modifiers: 2,
+};
+const paste = {
+  type: "keyDown",
+  windowsVirtualKeyCode: 86,
+  modifiers: 2,
+};
+
 chrome.commands.onCommand.addListener((command) => {
-  switch (command) {
-    case "runSomething":
-      (async function () {
-        const tab = await t();
-        const allSelect = {
-          type: "keyDown",
-          windowsVirtualKeyCode: 65,
-          modifiers: 4,
-        };
-        const paste = {
-          type: "keyDown",
-          key: "v",
-          code: "KeyV",
-          location: 1,
-          windowsVirtualKeyCode: 86,
-          modifiers: 4,
-        };
+  if (command == "format") {
+    (async function () {
+      // アクティブタブの情報を取得
+      const tab = await t();
 
-        await chrome.debugger.attach({ tabId: tab.id }, "1.3");
+      // colabのサイト以外は以降の処理をスキップ
+      if (tab.url.indexOf("https://colab.research.google.com/")) {
+        return;
+      }
 
-        await chrome.debugger.sendCommand(
-          { tabId: tab.id },
-          "Input.dispatchKeyEvent",
-          allSelect
-        );
-        const code = await chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          func: () => {
-            return window.getSelection().toString();
-          },
-        });
-        const options2 = {
-          method: "POST",
-          headers: {
-            accept: "application/json",
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({
-            text: code[0].result,
-          }),
-        };
-        const formattedCode = await fetch("http://127.0.0.1:4201", options2)
-          .then((response) => {
-            return response.json();
-          })
-          .catch((err) => console.error(err));
-        // chrome.debugger.sendCommand({ tabId: tab.id }, "Input.insertText", {
-        //   text: formattedCode.text,
-        // });
+      // デバッガをアタッチ
+      await chrome.debugger.attach({ tabId: tab.id }, "1.3");
 
-        // await chrome.scripting.executeScript({
-        //   target: { tabId: tab.id },
-        //   func: (code) => {
-        //     navigator.clipboard.writeText(code.text);
-        //   },
-        //   args: [formattedCode],
-        // });
-        await chrome.debugger.sendCommand(
-          { tabId: tab.id },
-          "Input.dispatchKeyEvent",
-          paste
-        );
-        await chrome.debugger.detach({ tabId: tab.id });
-      })();
-      break;
+      // コードを全選択し、クリップボードにコピー
+      await chrome.debugger.sendCommand(
+        { tabId: tab.id },
+        "Input.dispatchKeyEvent",
+        allSelect
+      );
+      await chrome.debugger.sendCommand(
+        { tabId: tab.id },
+        "Input.dispatchKeyEvent",
+        copy
+      );
+
+      // クリップボードの内容（コード）を読み取り
+      const code = await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          return navigator.clipboard.readText();
+        },
+      });
+
+      // フォーマット済のコードをapiから取得
+      const options = {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          text: code[0].result,
+        }),
+      };
+      const formattedCode = await fetch(
+        "https://colabformatter-1-l8242131.deta.app/",
+        options
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .catch((err) => console.error(err));
+
+      // フォーマット済のコードをクリップボードに書き込み
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: (code) => {
+          navigator.clipboard.writeText(code.text.replace(/\s$/, "")); // 最後の改行はnotebookにおいては邪魔なので削除
+        },
+        args: [formattedCode],
+      });
+
+      // クリップボードの内容をペースト
+      await chrome.debugger.sendCommand(
+        { tabId: tab.id },
+        "Input.dispatchKeyEvent",
+        paste
+      );
+
+      // デバッガをデタッチ
+      await chrome.debugger.detach({ tabId: tab.id });
+    })();
   }
 });
