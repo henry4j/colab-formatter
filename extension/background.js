e0f63c06d17c1c0757af8a7d6f1baa410ee34ce1
daiki_mac <kubotadaiki0654@gmail.com>
2023-09-20 00:13:40 +0900

    fix: macでもフォーマット済みコードをクリップボードに書き込む段階まではできるよう修正(#1)

--- extension/background.js
+++ extension/background.js
@@ -3,22 +3,11 @@ async function t() {
   return (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0];
 }
 
-// 入力するkey情報を設定
-const allSelect = {
-  type: "keyDown",
-  windowsVirtualKeyCode: 65,
-  modifiers: 2,
-};
-const copy = {
-  type: "keyDown",
-  windowsVirtualKeyCode: 67,
-  modifiers: 2,
-};
-const paste = {
-  type: "keyDown",
-  windowsVirtualKeyCode: 86,
-  modifiers: 2,
-};
+// OSの判別
+let os = "";
+chrome.runtime.getPlatformInfo(function (info) {
+  os = info.os;
+});
 
 chrome.commands.onCommand.addListener((command) => {
   if (command == "format") {
@@ -31,30 +20,54 @@ chrome.commands.onCommand.addListener((command) => {
         return;
       }
 
+      //OSによって修飾キーを変える
+      const ctrl = 2;
+      const command = 4;
+      let modifiers = os == "mac" ? command : ctrl;
+
+      // 入力するkey情報を設定
+      const allSelect = {
+        type: "keyDown",
+        windowsVirtualKeyCode: 65,
+        modifiers: modifiers,
+      };
+      const paste = {
+        type: "keyDown",
+        windowsVirtualKeyCode: 86,
+        modifiers: 2,
+      };
+
       // デバッガをアタッチ
       await chrome.debugger.attach({ tabId: tab.id }, "1.3");
 
-      // コードを全選択し、クリップボードにコピー
+      // コードを全選択
       await chrome.debugger.sendCommand(
         { tabId: tab.id },
         "Input.dispatchKeyEvent",
         allSelect
       );
-      await chrome.debugger.sendCommand(
-        { tabId: tab.id },
-        "Input.dispatchKeyEvent",
-        copy
-      );
 
-      // クリップボードの内容（コード）を読み取り
+      // フォーマット前のコードを取得
       const code = await chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: () => {
-          return navigator.clipboard.readText();
+          const code = document.querySelectorAll(
+            ".cell.code.focused .view-line"
+          );
+          let combinedCode = "";
+          code.forEach((line) => {
+            const lineChildren = [...line.children[0].children];
+            let combinedLine = "";
+            lineChildren.forEach((child) => {
+              combinedLine += child.textContent;
+            });
+            combinedCode += combinedLine + "\n";
+          });
+          return combinedCode;
         },
       });
 
-      // フォーマット済のコードをapiから取得
+      // フォーマット済みのコードをapiから取得
       const options = {
         method: "POST",
         headers: {
@@ -62,7 +75,7 @@ chrome.commands.onCommand.addListener((command) => {
           "content-type": "application/json",
         },
         body: JSON.stringify({
-          text: code[0].result,
+          text: code[0].result.replace(/\xA0/g, " "), // ノーブレークスペースがあるとフォーマットが正しくできないので通常の半角スペースに変換する
         }),
       };
       const formattedCode = await fetch(
@@ -74,7 +87,7 @@ chrome.commands.onCommand.addListener((command) => {
         })
         .catch((err) => console.error(err));
 
-      // フォーマット済のコードをクリップボードに書き込み
+      // フォーマット済みのコードをクリップボードに書き込み
       await chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: (code) => {
