e07f853f692da66023792e3f437e777b0f40a4e6
daiki_win <kubotadaiki0654@gmail.com>
2024-02-20 18:07:21 +0900

    refactor: backgroundのリファクタリング
    クラスに抽出するなど、全体的なリファクタリングをした。

--- src/background.tsx
+++ src/background.tsx
@@ -1,137 +1,40 @@
-// offscreenの作成
+// コードフォーマット用のoffscreenを作成
 chrome.offscreen.createDocument({
   url: "offscreen.html",
   reasons: [chrome.offscreen.Reason.IFRAME_SCRIPTING],
   justification: "reason for needing the document",
 });
 
-const ctrl = 2;
-const command = 4;
-const shift = 8;
-
-const paste = {
-  type: "keyDown",
-  windowsVirtualKeyCode: 86,
-  modifiers: ctrl,
-};
-const copy = {
-  type: "keyDown",
-  windowsVirtualKeyCode: 67,
-  modifiers: ctrl,
-};
-const deleteKey = {
-  type: "keyDown",
-  windowsVirtualKeyCode: 8,
-};
-const selectIndent = {
-  type: "keyDown",
-  windowsVirtualKeyCode: 36,
-  modifiers: shift,
-};
-
+// ショートカットキー入力時に以下の関数が実行される
 chrome.commands.onCommand.addListener((commands, tab) => {
   if (commands == "format") {
     (async function () {
-      // 特定ののサイト以外は以降の処理をスキップ
-      if (
-        tab.url?.indexOf("https://colab.research.google.com/") &&
-        tab.url.indexOf("https://www.kaggle.com/")
-      ) {
+      // フォーマットをする必要が無いサイトでは以降の処理をスキップ
+      const permittedUrl = ["https://colab.research.google.com/", "https://www.kaggle.com/"]
+      const isPermitted = permittedUrl.some((url) => { return tab.url?.indexOf(url) === 0; });
+      if (!isPermitted) {
         return;
       }
-      // OSの判別
+
+      // chrome.debugger APIを有効にする
+      await chrome.debugger.attach({ tabId: tab.id }, "1.3");
+
+      // コードをフォーマットしてセルに書き込む
       const os = (await chrome.runtime.getPlatformInfo()).os;
+      const codeController = os === "mac" ? new MacCodeController(tab.id!) : new CodeController(tab.id!)
+      const code = await codeController.readCode();
+      const formattedCode = await formatCode(code);
+      await codeController.writeCode(formattedCode);
 
-      const modifiers = os == "mac" ? command : ctrl; //OSによって修飾キーを変える
-
-      const allSelect = {
-        type: "keyDown",
-        windowsVirtualKeyCode: 65,
-        modifiers: modifiers,
-      };
-
-      await chrome.debugger.attach({ tabId: tab.id }, "1.3"); // デバッガをアタッチ
-
-      await pressKey(tab.id, allSelect);
-
-      let code;
-      if (os == "mac") {
-        code = await copyCodeMac(tab.id);
-      } else {
-        code = await copyCode(tab.id);
-      }
-
-      let formattedCode = await formatCode(code);
-
-      if (os == "mac") {
-        await pasteCodeMac(tab.id, code, formattedCode);
-      } else {
-        await pasteCode(tab.id, formattedCode);
-      }
-
-      await chrome.debugger.detach({ tabId: tab.id }); // デバッガをデタッチ
+      // chrome.debugger APIを無効にする
+      await chrome.debugger.detach({ tabId: tab.id });
     })();
   }
 });
 
-/**
- * テキストをペーストする
- *
- * @param {*} tabId ペースト先のタブ
- * @param {*} text ペーストするテキスト
- */
-async function pasteCode(tabId: any, text: any) {
-  await chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    func: (text) => {
-      navigator.clipboard.writeText(text);
-    },
-    args: [text],
-  });
 
-  await pressKey(tabId, paste);
-}
-
-/**
- * テキストをペーストする（Mac用）
- *
- * @param {*} tabId ペースト先のタブ
- * @param {*} code フォーマット前のコード
- * @param {*} formattedCode フォーマット後のコード
- */
-async function pasteCodeMac(tabId: any, code: any, formattedCode: any) {
-  // フォーマット前のコードをクリップボードに書き込み
-  await chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    func: (code) => {
-      navigator.clipboard.writeText(code);
-    },
-    args: [code[0].result.replace(/\xA0/g, " ")],
-  });
-
-  //　コードを改行で分割
-  let splitCode = formattedCode.split(/(?<=\r\n|\n)/);
-
-  await insertText(tabId, splitCode[0]);
-  for (let i = 1; i < splitCode.length; i++) {
-    // 前の行にインデントがある場合は、自動インデントを削除する
-    if (splitCode[i - 1].indexOf(" ") == 0) {
-      await pressKey(tabId, selectIndent);
-      await pressKey(tabId, deleteKey);
-    }
-
-    await insertText(tabId, splitCode[i]);
-  }
-}
-
-/**
- * offscreenにコードを送り、フォーマットする
- *
- * @param {*} code フォーマットするコード
- * @return {*} フォーマット済みのコード
- */
-async function formatCode(code: any) {
-  const response = await chrome.runtime.sendMessage({ code: code[0].result });
+async function formatCode(code: string) {
+  const response = await chrome.runtime.sendMessage({ code: code }); // offscreenにコードを送り、フォーマットする
   if (response.status === "error") {
     chrome.notifications.create("", {
       title: "colab-formatter",
@@ -143,82 +46,151 @@ async function formatCode(code: any) {
   return response.code;
 }
 
-/**
- * テキストをコピーする
- *
- * @param {*} tabId コピー元のタブID
- * @return {*} コピーしたテキスト
- */
-async function copyCode(tabId: any) {
-  await pressKey(tabId, copy);
-  const text = await chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    func: async () => {
-      return await navigator.clipboard.readText();
-    },
-  });
-  return text;
+class CodeController {
+  readonly tabId: number;
+  key: Key;
+
+  constructor(tabId: number) {
+    this.tabId = tabId;
+    this.key = new Key(tabId, false);
+  }
+
+  async readCode() {
+    await this.key.press("allSelect")
+    await this.key.press("copy")
+    const code = await chrome.scripting.executeScript({
+      target: { tabId: this.tabId },
+      func: async () => {
+        return await navigator.clipboard.readText();
+      },
+    });
+    return code[0].result;
+  }
+
+  async writeCode(code: string) {
+    await chrome.scripting.executeScript({
+      target: { tabId: this.tabId },
+      func: (text) => {
+        navigator.clipboard.writeText(text);
+      },
+      args: [code],
+    });
+    await this.key.press("paste")
+  }
 }
 
-/**
- * テキストをペーストする（Mac用）
- *
- * @param {*} tabId コピー元のタブID
- * @return {*} コピーしたテキスト
- */
-async function copyCodeMac(tabId: any) {
-  const code = await chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    func: () => {
-      const code = [
-        ...document.querySelectorAll<HTMLElement>(
-          ".cell.code.focused .view-line"
-        ),
-      ];
-      let combinedCode = "";
+class MacCodeController {
+  readonly tabId: number;
+  key: Key;
 
-      code.sort(function (first, second) {
-        const firstStyleTop = Number(first.style.top.replace("px", ""));
-        const secondStyleTop = Number(second.style.top.replace("px", ""));
-        return firstStyleTop - secondStyleTop;
-      });
+  constructor(tabId: number) {
+    this.tabId = tabId;
+    this.key = new Key(tabId, true);
+  }
 
-      code.forEach((line) => {
-        const lineChildren = [...line.children[0].children];
-        let combinedLine = "";
-        lineChildren.forEach((child) => {
-          combinedLine += child.textContent;
+  async readCode() {
+    await this.key.press("allSelect")
+
+    const code = await chrome.scripting.executeScript({
+      target: { tabId: this.tabId },
+      func: () => {
+        // コードに関連するHTML要素のリストを取得
+        const code = [
+          ...document.querySelectorAll<HTMLElement>(
+            ".cell.code.focused .view-line"
+          ),
+        ];
+
+        // リストの順番がコードの順番と同じとは限らないため、正しい順番にソートする
+        // CSSのtopの値が正しいコードの順番になっているようなので、topの値を基準にソートしている
+        code.sort(function (first, second) {
+          const firstStyleTop = Number(first.style.top.replace("px", ""));
+          const secondStyleTop = Number(second.style.top.replace("px", ""));
+          return firstStyleTop - secondStyleTop;
         });
-        combinedCode += combinedLine + "\n";
-      });
-      return combinedCode;
-    },
-  });
-  return code;
+
+        // リストからコード本体を取得
+        let combinedCode = "";
+        code.forEach((line) => {
+          const lineChildren = [...line.children[0].children];
+          let combinedLine = "";
+          lineChildren.forEach((child) => {
+            combinedLine += child.textContent;
+          });
+          combinedCode += combinedLine + "\n";
+        });
+        return combinedCode;
+      },
+    });
+    return code[0].result;
+  }
+
+  async writeCode(code: string) {
+    const splitCode = code.split(/(?<=\r\n|\n)/);
+    await this.insertText(splitCode[0]);
+
+    for (let i = 1; i < splitCode.length; i++) {
+      // 前の行にインデントがある場合は、改行時にエディタ側で挿入される自動インデントを削除する
+      if (splitCode[i - 1].indexOf(" ") == 0) {
+        await this.key.press("selectIndent")
+        await this.key.press("deleteKey")
+      }
+
+      await this.insertText(splitCode[i]);
+    }
+  }
+
+  async insertText(text: string) {
+    await chrome.debugger.sendCommand({ tabId: this.tabId }, "Input.insertText", {
+      text: text,
+    });
+  }
 }
 
-/**
- * キーを押す動作をエミュレートする
- *
- * @param {*} tabId キーを押す先のタブID
- * @param {*} key 押すキー
- */
-async function pressKey(tabId: any, key: any) {
-  await chrome.debugger.sendCommand(
-    { tabId: tabId },
-    "Input.dispatchKeyEvent",
-    key
-  );
-}
+class Key {
+  readonly tabId: number;
+  readonly keyCommandParams;
 
-/**
- * テキストを挿入する
- *
- * @param {*} tabId 挿入先のタブID
- * @param {*} text 挿入するテキスト
- */
-async function insertText(tabId: any, text: any) {
-  await chrome.debugger.sendCommand({ tabId: tabId }, "Input.insertText", {
-    text: text,
-  });
+  constructor(tabId: number, isMac: boolean) {
+    const ctrl = 2;
+    const command = 4;
+    const shift = 8;
+    const modifiers = isMac ? command : ctrl; // OSによって修飾キーを変える
+
+    this.tabId = tabId
+    this.keyCommandParams = {
+      paste: {
+        type: "keyDown",
+        windowsVirtualKeyCode: 86,
+        modifiers: ctrl,
+      },
+      copy: {
+        type: "keyDown",
+        windowsVirtualKeyCode: 67,
+        modifiers: ctrl,
+      },
+      deleteKey: {
+        type: "keyDown",
+        windowsVirtualKeyCode: 8,
+      },
+      selectIndent: {
+        type: "keyDown",
+        windowsVirtualKeyCode: 36,
+        modifiers: shift,
+      },
+      allSelect: {
+        type: "keyDown",
+        windowsVirtualKeyCode: 65,
+        modifiers: modifiers,
+      }
+    }
+  }
+
+  async press(key: keyof typeof this.keyCommandParams) {
+    await chrome.debugger.sendCommand(
+      { tabId: this.tabId },
+      "Input.dispatchKeyEvent",
+      this.keyCommandParams[key]
+    );
+  }
 }
